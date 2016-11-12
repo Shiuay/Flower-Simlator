5511bb347d273afa001884205818fee5dc1022c8
STMiki <micou.veillet@gmail.com>
2016-11-12 02:11:24 +0100

    suppression de mon calendrier :'(
    MAIS NON ! IL EST DE RETOUR DANS LA CLASS MAIN_FENETRE !!! :heart:
    :heart:

--- bin/calendrier.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Calendrier.itemconfig("10", text="42", fill='black') # pour les texte. tags : [10, 51]
-# Calendrier.itemconfig("52", width=1) # pour les cadres. tags : [52, 93]
-
